a4c6f03e8cf159300592bcd0f5f8a2912461845d
GreenTechWiz <aggreen0405@gmail.com>
2019-02-11 22:53:18 -0600

    all

---
+++ fullname.rb
@@ -0,0 +1,5 @@
+puts "First name please"
+f_name = gets.chomp
+puts "Last name please"
+l_name = gets.chomp 
+puts f_name + " " + l_name
---
+++ number_multiplier.rb
@@ -0,0 +1,5 @@
+puts "First Number"
+f_num = gets.chomp.to_i
+puts "Second Number"
+l_num = gets.chomp.to_i
+puts f_num * l_num 
