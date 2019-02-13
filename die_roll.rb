135231b8f12cd4d8e6a37811fddc278b401bca4a
GreenTechWiz <47511977+GreenTechWiz@users.noreply.github.com>
2019-02-13 17:37:07 -0600

    Add files via upload

---
+++ die_roll.rb
@@ -0,0 +1,7 @@
+def die_roll
+  a = [1, 2, 3, 4, 5, 6]
+  puts a.sample
+  return
+end 
+
+die_roll
---
+++ last.rb
@@ -0,0 +1,5 @@
+def last(array)
+  array[array.length - 1]
+end
+
+puts last([1, 2, 3, 7, 5, 6, 2].sort)
